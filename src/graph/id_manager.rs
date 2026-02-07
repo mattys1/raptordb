2b9a34de3887dbd114279a2a93e29feb53367b7a
mattys1 <mattys0082@gmail.com>
2026-02-07 21:05:32 +0100

    bool consts

--- src/graph/id_manager.rs
+++ src/graph/id_manager.rs
@@ -3,11 +3,15 @@ use std::marker::PhantomData;
 use bitvec::prelude::*;
 use crate::graph::ID;
 
+const TAKEN: bool = true;
+const AVAILABLE: bool = false;
+
 #[derive(Debug)]
 pub struct IDManager<T> {
     ids: BitVec,
     _marker: PhantomData<T>,
 }
+
 
 impl<T: From<usize> + Copy + ID> IDManager<T> {
     pub fn new() -> Self {
@@ -22,24 +26,33 @@ impl<T: From<usize> + Copy + ID> IDManager<T> {
             Some(idx) => {
                 unsafe {
                     let mut bit = self.ids.get_unchecked_mut(idx);
-                    *bit = true;
+                    *bit = TAKEN;
                 }
 
                 idx.into()
             },
             None => {
-                self.ids.push(true);
+                self.ids.push(TAKEN);
                 (self.ids.len() - 1).into()
             }
         }
     }
 
-    pub fn add(&mut self, id: T) {
+    pub fn mark_as_taken(&mut self, id: T) {
         assert!(self.ids.len() > id.get_inner(), "tried to add id bigger than the graph");
 
         unsafe {
             let mut bit = self.ids.get_unchecked_mut(id.get_inner());
-            *bit = true;
+            *bit = TAKEN;
         }
+    }
+
+    pub fn is_taken(&self, id: T) -> bool {
+        assert!(self.ids.len() > id.get_inner(), "tried to check for id bigger than the graph");
+        // if id.get_inner() >= self.ids.len() {
+        //     return AVAILABLE;
+        // }
+
+        self.ids[id.get_inner()]
     }
 }
